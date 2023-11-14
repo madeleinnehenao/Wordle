370a465075b964f5b7028b063fe99e6e0e46ed60
Mad@23 <124703743+madeleinnehenao@users.noreply.github.com>
2023-11-13 19:31:27 -0500

    Add files via upload

---
+++ Datos.py
@@ -0,0 +1,66 @@
+class data4:
+    def __init__(self,initial_data=None):
+        if initial_data is None:
+            initial_data=set()
+        self.data=initial_data
+    
+    def words4(self):
+        with open("palabras_validas4.txt","r") as archivo:
+            for linea in archivo:
+                palabra=linea.strip()
+                self.data.add(palabra)
+        
+        return self.data
+class data5:
+    def __init__(self,initial_data=None):
+        if initial_data is None:
+            initial_data=set()
+        self.data=initial_data
+
+    def words5(self):
+        with open("palabras_validas5.txt","r") as archivo:
+            for linea in archivo:
+                palabra=linea.strip()
+                self.data.add(palabra)
+        return self.data
+
+class data6:
+    def __init__(self,initial_data=None):
+        if initial_data is None:
+            initial_data=set()
+        self.data=initial_data
+   
+    def words6(self):
+        with open("palabras_validas6.txt","r") as archivo:
+            for linea in archivo:
+                palabra=linea.strip()
+                self.data.add(palabra)
+        
+        print(self.data) 
+class data7:
+    def __init__(self,initial_data=None):
+        if initial_data is None:
+            initial_data=set()
+        self.data=initial_data
+    def words7(self):
+        with open("palabras_validas7.txt","r") as archivo:
+            for linea in archivo:
+                palabra=linea.strip()
+                self.data.add(palabra)
+        
+        return self.data
+
+class data8:
+    def __init__(self,initial_data=None):
+        if initial_data is None:
+            initial_data=set()
+        self.data=initial_data  
+        
+    def words8(self):
+        with open("palabras_validas8.txt","r") as archivo:
+            for linea in archivo:
+                palabra=linea.strip()
+                self.data.add(palabra)
+        
+        return self.data
+        
